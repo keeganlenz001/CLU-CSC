56f317adbce982ad91d39245ff9d2ee0b050eb26
Keegan Lenz <Keeganlenz12@gmail.com>
2021-09-21 18:15:18 -0700

    Reorganized exercises

--- CSC110/Exercises/Sum-series.py
+++
@@ -1,9 +0,0 @@
-num_of_nums = int(input('How many numbers would you like to be summed? '))
-result = 0
-
-for i in range(num_of_nums):
-    print('Please enter number', i + 1)
-    num = float(input())
-    result = result + num
-
-print(result)
--- CSC110/Exercises/Tri-area.py
+++
@@ -1,6 +0,0 @@
-base = float(input('What is the base of your triangle?  '))
-height = float(input('What is the height of your triangle?  '))
-
-area = (base * height) / 2
-
-print(area)
