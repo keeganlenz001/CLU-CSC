2280cba3a47b5cbccaa92f87ded84cd2a1f41540
Keegan Lenz <Keeganlenz12@gmail.com>
2021-10-04 21:59:23 -0700

    Finished HW 5

--- CSC110/HW/HW5-KL.py
+++ CSC110/HW/HW5-KL.py
@@ -2,35 +2,73 @@
 # Date: 9/30/2021
 # Project Description: Helps user practice arithmetic
 import random
+difficulty = ''
+operator = ''
 
 num1 = 0
 num2 = 0
 answer = 0
 
-difficulty = input('Would you like a Easy, Medium, or Hard problem?')
 
-operator = input('What operator would you to practice with (+, -, *, /, %)')
+def get_difficulty():
+    global difficulty
+
+    difficulty = input('Would you like a Easy, Medium, or Hard problem? ')
+    if difficulty != 'Easy' and difficulty != 'Medium' and difficulty != 'Hard':
+        print('Make sure you spelled your difficulty correctly (Case Sensitive)')
+        get_difficulty()
+    get_operator()
 
 
-def add():
-    if difficulty == 1:
-        global num1
-        global num2
-        global answer
-        num1 = random.randrange(100, 200)
-        num2 = random.randrange(100, 200)
+def get_operator():
+    global operator
+
+    operator = input('What operator would you to practice with (+, -, *, /, %) ')
+    if operator != '+' and operator != '-' and operator != '*' and operator != '/' and operator != '%':
+        print('Please enter a valid operator!')
+        get_operator()
+    calc()
+
+
+def calc():
+    global num1
+    global num2
+    global answer
+
+    if difficulty == 'Easy':
+        num1 = random.randrange(0, 10)
+        num2 = random.randrange(0, 10)
+    elif difficulty == 'Medium':
+        num1 = round(random.uniform(0, 100), 2)
+        num2 = round(random.uniform(0, 100), 2)
+    elif difficulty == 'Hard':
+        num1 = round(random.uniform(0, 1000), 2)
+        num2 = round(random.uniform(0, 1000), 2)
+
+    if operator == '+':
         answer = num1 + num2
-        print('What is', num1, '+', num2)
+    elif operator == '-':
+        answer = num1 - num2
+    elif operator == '*':
+        answer = num1 * num2
+    elif operator == '/':
+        answer = num1 / num2
+    elif operator == '%':
+        answer = num1 % num2
+
+    answer = round(answer, 2)
+    result()
 
 
+def result():
+    print('What is', num1, operator, num2)
+    print(answer)
+    user_answer = float(input())
 
-if operator == '+':
-    add()
-
-user_answer = input()
-if user_answer == answer:
-    print('Good job, you got it right!')
-else:
-    print('Sorry, that is the wrong answer')
+    if user_answer == answer:
+        print('Good job, you got it right!')
+    else:
+        print('Sorry, that is the wrong answer')
 
 
+get_difficulty()
